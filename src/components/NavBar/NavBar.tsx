ae36604d8162cdf52d3b7e0259577f845f16be1b
Fernanda Marques <f3rnandamarques@gmail.com>
2024-12-26 22:48:22 -0300

    feat: creates NavBar component

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,26 @@
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+const NavBar = () => {
+
+    const StyledToolBar = styled(Toolbar)(({ theme }) => ({
+        display: "flex",
+        justifyContent: "space-evenly",
+
+    }))
+
+    return (
+        <>
+
+            <AppBar position="absolute">
+                <StyledToolBar>
+                    <MenuItem>About</MenuItem>
+                    <MenuItem>Skills</MenuItem>
+                    <MenuItem>Projects</MenuItem>
+                </StyledToolBar>
+            </AppBar>
+
+        </>
+    )
+}
+
+export default NavBar
